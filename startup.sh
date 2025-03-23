0638e82224c83966661e7c91ed26938c2509c889
amilath79 <125643584+amilath79@users.noreply.github.com>
2025-03-23 19:34:15 +0100

    Fix FastAPI docs URL and enable Swagger UI 6

---
+++ startup.sh
@@ -0,0 +1,8 @@
+# Install ODBC Driver
+curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+curl https://packages.microsoft.com/config/debian/10/prod.list > /etc/apt/sources.list.d/mssql-release.list
+apt-get update
+ACCEPT_EULA=Y apt-get install -y msodbcsql17
+
+# Start the application
+gunicorn -w 4 -k uvicorn.workers.UvicornWorker apiagg:app
